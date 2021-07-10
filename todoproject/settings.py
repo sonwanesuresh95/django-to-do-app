aa33a24a96c6c25982ba32f6f1b76213fb112628
Suresh Sonwane <sonwanesuresh95@gmail.com>
2021-07-10 20:25:54 +0530

    aws mysql instance

--- todoproject/settings.py
+++ todoproject/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'todoproject.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql', 
+        'NAME': 'tasksdb',
+        'USER': 'admin',
+        'PASSWORD': 'adminadmin',
+        'HOST': 'tasksdb.cjzzi7msztzp.ap-south-1.rds.amazonaws.com',   # Or an IP Address that your DB is hosted on
+        'PORT': '3306',
     }
 }
 
